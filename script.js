5675dec593630e2a56d02769041fb2acd41c6eb1
Robin <mohammadrobin636@gmail.com>
2023-02-16 16:19:37 +0600

    done

--- script.js
+++ script.js
@@ -1,37 +1,40 @@
-
 // All Elements
-const addToCartButtons = document.getElementsByClassName('addToCart');
-const tableBody = document.getElementById('tableBody');
-let theTotal = document.getElementById('theTotal'); 
-
-
+const addToCartButtons = document.getElementsByClassName("addToCart");
+const tableBody = document.getElementById("tableBody");
+let theTotal = document.getElementById("theTotal");
 
 // Variables for calculation
-let number = 1;
+let number = 0;
 let allTotal = 0;
 
 // Event Listener
-document.getElementById('tableFooter').style.visibility = 'hidden';
-for(const btn of addToCartButtons) {
-    btn.addEventListener('click', function(){
-        document.getElementById('tableFooter').style.visibility = 'visible';
-        const theCard = this.parentNode.parentNode;
-        const name = theCard.querySelector('#name');
-        const price = theCard.querySelector('#price');
-        const quantity = theCard.querySelector('#piece');
-        let total = parseFloat(price.innerText) * parseFloat(quantity.value);
-        number++;
-        allTotal += total;
-        theTotal.innerText = allTotal;
-        tableBody.innerHTML += `
-            <tr>
-                <td>${number}</td>
-                <td>${name.innerText}</td>
-                <td>${price.innerText}</td>
-                <td>${quantity.value}</td>
-                <td>${total}</td>
-            </tr>
-        `
-    })
+document.getElementById("tableFooter").style.visibility = "hidden";
+for (const btn of addToCartButtons) {
+  btn.addEventListener("click", function () {
+    UpdateTableOfProducts(this);
+    if (this.parentNode.parentNode) {
+    }
+  });
 }
 
+
+function UpdateTableOfProducts(self) {
+  document.getElementById("tableFooter").style.visibility = "visible";
+  const theCard = self.parentNode.parentNode;
+  const name = theCard.querySelector("#name");
+  const price = theCard.querySelector("#price");
+  const quantity = theCard.querySelector("#piece");
+  let total = parseFloat(price.innerText) * parseFloat(quantity.value);
+  number++;
+  allTotal += total;
+  theTotal.innerText = allTotal;
+  tableBody.innerHTML += `
+        <tr>
+            <td>${number}</td>
+            <td>${name.innerText}</td>
+            <td>${price.innerText}</td>
+            <td>${quantity.value}</td>
+            <td>${total}</td>
+        </tr>
+    `;
+}
